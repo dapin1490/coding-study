01c37e1380f32b653ea0d7b5050613c4cdc06db1
dapin <89961651+dapin1490@users.noreply.github.com>
2021-12-06 16:48:36 +0900

    Update Euclidean algorithm.cpp
    일단 재귀함수가 아닌 형태로 썼음. 재귀함수로 하려면 다시 고민해야 함.

--- Euclidean algorithm.cpp
+++ Euclidean algorithm.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-  // 잠깐만 비주얼 스튜디오 먼저 해보고 공부도 좀 더 하고
+int euclidean(int big, int small);
+
+int main() { 
+	
+	int a, b;
+	cout << "두 수 입력 : ";
+	cin >> a >> b;
+	cout << "\n" << a << "와 " << b << "의 최대공약수는 " << euclidean(a, b) << "이다." << endl;
+
+	
+	return 0;
 }
+
+int euclidean(int big, int small)
+{
+  int last = 1; // 매 연산마다 나머지를 저장함
+  
+  while (last != 0)
+  {
+    if (big % small == 0)
+      break;
+    last = big % small;
+    big = small;
+    small = last;
+  }
+  
+  return last;
+}
+
+
