08ace2836ff8171bf61dad8440a08946d4faed17
dapin <89961651+dapin1490@users.noreply.github.com>
2022-01-21 17:10:36 +0900

    Update 9020 골드바흐의 추측.cpp

--- C++/9020 골드바흐의 추측.cpp
+++ C++/9020 골드바흐의 추측.cpp
@@ -4,10 +4,14 @@
 using namespace std;
 
 vector<int> v(10001); // 소수 여부 저장
-string gold(int n);
 
 int main()
 {
+	// 빠른 입출력
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
 	v[0] = 1; // 소수가 아니니까 1로 초기화
 	v[1] = 1;
 
@@ -15,39 +19,31 @@ int main()
 	for (int i = 2; i < 10000 + 1; i++)
 	{
 		for (int j = 2 * i; j < 10000 + 1; j += i)
-		{
-			if (v[j] == 0)
-				v[j] = 1;
-		}
+			v[j] = 1;
 	}
 	
 	int t;
 	cin >> t;
 	for (int i = 0; i < t; i++)
 	{
-		int num;
-		cin >> num;
-		cout << gold(num) << "\n";
+		int n;
+		cin >> n;
+
+		int idx = n / 2;
+		int id2 = n - idx;
+		while (true)
+		{
+			if (v[idx] == 0 && v[id2] == 0)
+				break;
+			else
+			{
+				idx--;
+				id2++;
+			}
+		}
+		
+		cout << idx << " " << id2 << "\n";
 	}
 
 	return 0;
-}
-
-string gold(int n)
-{
-	int idx = n / 2;
-	int id2 = n - idx;
-	while (true)
-	{
-		if (v[idx] == 0 && v[id2] == 0)
-			break;
-		else
-		{
-			idx--;
-			id2++;
-		}
-	}
-
-	string ans = to_string(idx) + " " + to_string(id2);
-	return ans;
 }
