0815f5f5726fd2ff0a13d23511d933fc844f1b05
dapin <89961651+dapin1490@users.noreply.github.com>
2022-01-22 16:41:12 +0900

    Create 15649 N과 M (1).cpp

---
+++ C++/15649 N과 M (1).cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+bool nvi[] = { 0, 0, 0, 0, 0, 0, 0, 0 };
+vector<int> v;
+int mm, nn;
+
+void pr(int n, int m);
+
+int main()
+{
+	cin >> nn >> mm;
+
+	pr(nn, mm);
+
+	return 0;
+}
+
+void pr(int n, int m)
+{
+	if (v.size() == m)
+	{
+		for (int i = 0; i < v.size(); i++)
+			cout << v[i] << " ";
+		cout << "\n";
+		return;
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		if (nvi[i])
+			continue;
+		v.push_back(i + 1);
+		nvi[i] = 1;
+		pr(n, m);
+		v.pop_back();
+		nvi[i] = 0;
+	}
+}
