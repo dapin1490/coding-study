1ce69e946b112c6dc62a3b910ad4d92498062432
dapin <89961651+dapin1490@users.noreply.github.com>
2022-01-23 22:30:50 +0900

    Create 15651 N과 M (3).py

---
+++ 파이썬/15651 N과 M (3).py
@@ -0,0 +1,25 @@
+nvi = [0, 0, 0, 0, 0, 0, 0, 0]
+v = []
+
+
+def pr(n, m):
+    if len(v) == m:
+        for k in v:
+            print(k, end=" ")
+        print()
+        return
+
+    for i in range(n):
+        if nvi[i] >= m:
+            continue
+        v.append(i + 1)
+        nvi[i] += 1
+        pr(n, m)
+        idx = len(v)
+        del v[idx - 1]
+        nvi[i] -= 1
+
+
+if __name__ == '__main__':
+    nn, mm = map(int, input().split())
+    pr(nn, mm)
