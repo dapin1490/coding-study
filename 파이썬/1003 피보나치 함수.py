6fb9a3be982c24c83dfb96488cb4b80be43e1108
dapin <89961651+dapin1490@users.noreply.github.com>
2022-01-31 16:49:29 +0900

    Update 1003 피보나치 함수.py

--- 파이썬/1003 피보나치 함수.py
+++ 파이썬/1003 피보나치 함수.py
@@ -1,5 +1,6 @@
 # 피보나치 수열 일반항 사용
 # 출처 : https://shoark7.github.io/programming/algorithm/%ED%94%BC%EB%B3%B4%EB%82%98%EC%B9%98-%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98%EC%9D%84-%ED%95%B4%EA%B2%B0%ED%95%98%EB%8A%94-5%EA%B0%80%EC%A7%80-%EB%B0%A9%EB%B2%95
+# 출처의 출처 : http://suhak.tistory.com/81
 
 
 def fibo(n):
