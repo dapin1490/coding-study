404f909253f24d85ad1556b9739d0a31ec299b14
dapin <89961651+dapin1490@users.noreply.github.com>
2022-02-09 17:27:29 +0900

    Create 1934 최소공배수.cpp

---
+++ C++/1934 최소공배수.cpp
@@ -0,0 +1,50 @@
+/* 유클리드 호제법 : 최대공약수를 구할 수 있다
+최소공배수 : 두 수의 곱 ÷ 최대공약수 */
+
+#include <iostream>
+using namespace std;
+
+int euclidean(int big, int small);
+
+int main()
+{
+	// 빠른 입출력
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int t;
+	cin >> t;
+
+	for (int i = 0; i < t; i++) {
+		int a, b;
+		cin >> a >> b;
+
+		int n = euclidean(a, b);
+		cout << a * b / n << "\n";
+	}
+
+	return 0;
+}
+
+int euclidean(int big, int small)
+{
+	if (big < small) { // 대소관계 정리
+		int temp = big;
+		big = small;
+		small = temp;
+	}
+
+	int last = 1; // 매 연산마다 나머지를 저장함
+
+	while (true)
+	{
+		if (big % small == 0)
+			break;
+		last = big % small;
+		big = small;
+		small = last;
+	}
+
+	return small;
+}
