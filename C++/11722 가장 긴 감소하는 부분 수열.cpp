79e969076f8efb9553d7d4c00603f67d23b829ad
dapin <89961651+dapin1490@users.noreply.github.com>
2022-02-11 19:55:03 +0900

    Create 11722 가장 긴 감소하는 부분 수열.cpp

---
+++ C++/11722 가장 긴 감소하는 부분 수열.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// 증가하는 수열 거꾸로 찾기
+int finder(int n);
+
+vector<int> v;
+vector<int> d;
+int t, cnt = 1;
+
+int main()
+{
+	// 빠른 입출력
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int a;
+	cin >> t;
+	d.resize(t, 1);
+	for (int i = 0; i < t; i++) {
+		cin >> a;
+		v.push_back(a);
+	}
+
+	a = t - 1;
+	while (a >= 0) {
+		int k = finder(a);
+		if (cnt < k) {
+			cnt = k;
+		}
+		a--;
+	}
+
+	cout << cnt;
+
+	return 0;
+}
+
+// 증가하는 수열 거꾸로 찾기
+int finder(int n)
+{
+	for (int i = n; i < t; i++) {
+		if (v[i] < v[n] && d[i] >= d[n]) {
+			d[n] = d[i] + 1;
+		}
+	}
+
+	return d[n];
+}
