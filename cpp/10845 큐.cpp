475f8263e624cd8e7e70fb49b6d22b486ce419fd
dapin <89961651+dapin1490@users.noreply.github.com>
2022-03-19 19:30:30 +0900

    Create 10845 큐.cpp

---
+++ cpp/10845 큐.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+using namespace std;
+
+vector<int> v;
+map<string, int> m;
+
+int main()
+{
+	// 빠른 입출력
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+	m.insert(pair<string, int>("push", 0));
+	m.insert(pair<string, int>("pop", 1));
+	m.insert(pair<string, int>("size", 2));
+	m.insert(pair<string, int>("empty", 3));
+	m.insert(pair<string, int>("front", 4));
+	m.insert(pair<string, int>("back", 5));
+
+	int t, num;
+	string query;
+
+	cin >> t;
+
+	for (int i = 0; i < t; i++) {
+		cin >> query;
+
+		switch (m[query]) {
+		case 0:
+			cin >> num;
+			v.push_back(num);
+			break;
+		case 1:
+			if (v.empty()) {
+				cout << -1;
+			}
+			else {
+				cout << v.front();
+				v.erase(v.begin());
+			}
+			cout << "\n";
+			break;
+		case 2:
+			cout << v.size() << "\n";
+			break;
+		case 3:
+			if (v.empty())
+				cout << 1;
+			else
+				cout << 0;
+			cout << "\n";
+			break;
+		case 4:
+			if (v.empty()) {
+				cout << -1;
+			}
+			else {
+				cout << v.front();
+			}
+			cout << "\n";
+			break;
+		case 5:
+			if (v.empty()) {
+				cout << -1;
+			}
+			else {
+				cout << v.back();
+			}
+			cout << "\n";
+			break;
+		}
+	}
+
+	return 0;
+}
