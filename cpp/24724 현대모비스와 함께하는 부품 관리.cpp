377bb71ebbfe49a4562164f3cfe95479b9986303
dapin <89961651+dapin1490@users.noreply.github.com>
2022-03-20 17:25:57 +0900

    Create 24724 현대모비스와 함께하는 부품 관리.cpp

---
+++ cpp/24724 현대모비스와 함께하는 부품 관리.cpp
@@ -0,0 +1,21 @@
+// 2022 연세대학교 신학기맞이 프로그래밍 경진대회 Open Contest 참가
+// B번 : 시간 내 성공
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	// 빠른 입출력
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+	int t;
+	
+	cin >> t;
+
+	for (int i = 1; i <= t; i++) {
+		cout << "Material Management " << i << "\nClassification ---- End!\n";
+	}
+
+	return 0;
+}
