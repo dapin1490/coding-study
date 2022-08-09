d27a3949032c2ccfc527654a61925cca8278ce99
dapin <89961651+dapin1490@users.noreply.github.com>
2022-08-10 00:45:30 +0900

    Create 9498 시험 성적.kt

---
+++ kotlin/9498 시험 성적.kt
@@ -0,0 +1,23 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.util.StringTokenizer
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+    val token = StringTokenizer(br.readLine())
+    var score = token.nextToken().toInt()
+    
+    when (score) {
+        in 90..100 -> bw.write("A\n")
+        in 80..89 -> bw.write("B\n")
+        in 70..79 -> bw.write("C\n")
+        in 60..69 -> bw.write("D\n")
+        else -> bw.write("F\n")
+    }
+    
+    bw.flush()
+    bw.close()
+}
